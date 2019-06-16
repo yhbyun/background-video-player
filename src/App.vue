d6b3068f5193267a7d5172b08059b73d88103692
YongHun Byun <yonghunbyun@gmail.com>
2019-06-16 23:42:06 +0900

    Add video playing feature

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-	<div id="app">
-		<img alt="Vue logo" src="./assets/logo.png">
-		<HelloWorld msg="Welcome to Your Vue.js App"/>
-	</div>
+    <div id="app">
+        <vue-plyr ref="plyr">
+            <video poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg" src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4">
+                <source src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4" type="video/mp4" size="720">
+                <source src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4" type="video/mp4" size="1080">
+                <track kind="captions" label="English" srclang="en" src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt" default>
+            </video>
+        </vue-plyr>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ipcRenderer } from 'electron'
 
 export default {
-	name: 'app',
-	components: {
-		HelloWorld
-	}
+    name: 'app',
+    computed: {
+        player () { return this.$refs.plyr.player },
+    },
+    mounted () {
+        ipcRenderer.on('play-control', (event, message) => {
+            if (message === 'play') {
+                this.player.play();
+            } else if (message === 'pause') {
+                this.player.pause();
+            }
+        })
+    },
 }
 </script>
 
 <style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
 </style>
